b8ffd142c012089a79f8c504dad41f7af8fbe689
youngchullim <youngchullim95@gmail.com>
2019-02-06 17:33:28 -0800

    Initial push of W5D3 project - Rails Lite

---
+++ skeleton/bin/p01_basic_server.rb
@@ -0,0 +1,19 @@
+require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new 
+
+  res['Content-Type'] = 'text/html'
+  if req.path == '/i/love/app/academy'
+    res.write("i/love/app/academy")
+  else
+    res.write("Hello World!")
+  end
+  res.finish
+end 
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
